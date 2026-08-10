771494b229972fe677e7c94f3a122a52fa97bcb1
gauranshp06-ctrl <gauranshp06@gmail.com>
2026-08-10 18:34:34 +0530

    Day1.c

---
+++ Day1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,c;
+  printf("Enter a ");
+  scanf("%d",&a);
+  printf("Enter b ");
+  scanf("%d",&b);
+  c=a+b;
+  printf("Addition is %d",c);
+  getch();
+}
+
+
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e;
+float f:
+clrscr();
+printf("Enter a ");
+scanf("%d",&a);
+printf("Enter b ");
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b:
+printf("\n Sum is %d",c);
+printf("\n Subtraction is %d",d);
+printf("\n Multiplication is %d",e):
+printf("\n Division is %f",f);
+getch();
+}
