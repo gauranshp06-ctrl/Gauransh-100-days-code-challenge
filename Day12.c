98dd05ab5eb2b8c95754c014c2b1449bc146fe00
gauranshp06-ctrl <gauranshp06@gmail.com>
2026-09-14 10:42:53 +0530

    Day12.c

---
+++ Day12.c
@@ -0,0 +1,64 @@
+Q23-
+  #include <stdio.h>
+
+int main()
+{
+    int days, fine;
+
+    printf("Enter late days: ");
+    scanf("%d", &days);
+
+    if (days <= 5)
+    {
+        fine = days * 2;
+        printf("Fine Rs.%d", fine);
+    }
+    else if (days <= 10)
+    {
+        fine = (5 * 2) + ((days - 5) * 4);
+        printf("Fine Rs.%d", fine);
+    }
+    else if (days <= 30)
+    {
+        fine = (5 * 2) + (5 * 4) + ((days - 10) * 6);
+        printf("Fine Rs.%d", fine);
+    }
+    else
+    {
+        printf("Membership Cancelled");
+    }
+
+    return 0;
+}
+
+Q24-
+  #include <stdio.h>
+
+int main()
+{
+    int units, bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100)
+    {
+        bill = units * 5;
+    }
+    else if (units <= 200)
+    {
+        bill = (100 * 5) + ((units - 100) * 7);
+    }
+    else if (units <= 300)
+    {
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+    }
+    else
+    {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 12);
+    }
+
+    printf("Bill: Rs.%d", bill);
+
+    return 0;
+}
